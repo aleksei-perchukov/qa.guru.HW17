976669b1cf5d6149f790b71dab73befabd398d4b
Aleksei Perchukov <aleksei.perchukov@gmail.com>
2022-10-06 22:18:54 +0300

    + 5 API tests

---
+++ src/test/java/ReqresInTestsAper.java
@@ -0,0 +1,110 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTestsAper {
+
+    /*
+        1. make POST- request to https://reqres.in/api/login
+            with body { "email": "eve.holt@reqres.in", "password": "pistol" }
+        2. get response { "id": 4, "token": "QpwL5tke4Pnpja7X4" }
+        3. check token is "QpwL5tke4Pnpja7X4"
+     */
+
+    @Test
+    void postRegisterSuccessful() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void postRegisterUnsuccessful() {
+        String body = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void putUpdate() {
+        String body = "{ \"name\": \"morpheus\", " +
+                "\"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void delete() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .statusCode(204);
+    }
+
+    @Test
+    void singleResource() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.name", is("fuchsia rose"))
+                .body("data.year", is(2001))
+                .body("data.color", is("#C74375"))
+                .body("data.pantone_value", is("17-2031"))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+    }
+}
+
